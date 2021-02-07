4dc0e35d4258d2dcf0479eebe7c4ce0c4f63b683
mona9992 <76748531+mona9992@users.noreply.github.com>
2021-02-07 19:31:58 +0330

    UISounds

---
+++ WakeUp/Assets/Scripts/TriggerAudio.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string Event;
+    public bool PlayOnAwake;
+
+    public void playOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached(Event, gameObject);
+    }
+
+
+    private void Start()
+    {
+        if (PlayOnAwake)
+            playOneShot();
+    }
+}
